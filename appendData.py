c3789906aefd078f6d86fb183cc9f183335bad1d
lyl2016 <574402032@qq.com>
2022-03-31 01:02:29 +0800

    202203310102 适配进度条显示

--- appendData.py
+++ appendData.py
@@ -1,31 +1,35 @@
 # -*- coding: UTF-8 -*-
 import os
-#设定基准目录
+from func import ProgressBar
+# 设定基准目录
 foldPath = "/Volumes/Seagate/下载/FinacialData/"
+# 遍历股票代码
+stockCodes = os.listdir(foldPath)
+progress = ProgressBar(len(stockCodes), fmt=ProgressBar.FULL)
+for stockCode in stockCodes:
+    lrbPath = foldPath + stockCode + "/" + stockCode + "_lrb.csv"  # 利润表路径
+    xjllbPath = foldPath + stockCode + "/" + stockCode + "_xjllb.csv"  # 现金流量表
+    zcfzbPath = foldPath + stockCode + "/" + stockCode + "_zcfzb.csv"  # 资产负债表
+    lines = []
+    with open(lrbPath, encoding='utf-8') as lrbContent:
+        for line in lrbContent:
+            lines.append(line)
+    with open(xjllbPath, encoding='utf-8') as xjllbContent:
+        xjllbContent.readline()
+        for line in xjllbContent:
+            lines.append(line)
+    with open(zcfzbPath, encoding='utf-8') as zcfzbContent:
+        zcfzbContent.readline()
+        for line in zcfzbContent:
+            lines.append(line)
 
-#遍历股票代码
-for stockCode in os.listdir(foldPath):
-	lrbPath = foldPath + stockCode + "/" + stockCode + "_lrb.csv"#利润表路径
-	xjllbPath = foldPath + stockCode + "/" + stockCode + "_xjllb.csv"#现金流量表
-	zcfzbPath = foldPath + stockCode + "/" + stockCode + "_zcfzb.csv"#资产负债表
-	lines = []
-	with open(lrbPath, encoding = 'utf-8') as lrbContent:
-		for line in lrbContent:
-			lines.append(line)
-	with open(xjllbPath, encoding = 'utf-8') as xjllbContent:
-		xjllbContent.readline()
-		for line in xjllbContent:
-			lines.append(line)
-	with open(zcfzbPath, encoding = 'utf-8') as zcfzbContent:
-		zcfzbContent.readline()
-		for line in zcfzbContent:
-			lines.append(line)
-	
-	#合并三个文件
-	fullTableFileName = foldPath + stockCode + "/" + stockCode + "_full.csv"
-	with open(fullTableFileName, "w", encoding = 'utf-8') as file2Write:
-		for line in lines:
-			if (line.split(",")[1].strip().upper() != "NAN"):
-				file2Write.write(line)
-		print("已写入文件" + fullTableFileName)
-print("执行完毕")
+    # 合并三个文件
+    fullTableFileName = foldPath + stockCode + "/" + stockCode + "_full.csv"
+    with open(fullTableFileName, "w", encoding='utf-8') as file2Write:
+        for line in lines:
+            if (line.split(",")[1].strip().upper() != "NAN"):
+                file2Write.write(line)
+
+    progress.current += 1
+    progress()
+progress.done()
--- expandFullTable.py
+++ expandFullTable.py
@@ -1,46 +1,51 @@
 import os
-#设定基准目录
+from func import ProgressBar
+# 设定基准目录
 foldPath = "/Volumes/Seagate/下载/FinacialData/"
-#遍历股票代码
-for stockCode in os.listdir(foldPath):
-	#表头 => 数据
-	KeyValuePair = {}
-	#将新生成的full表展开单行
-	fullTableFileName = foldPath + stockCode + "/" + stockCode + "_full.csv"
-	with open(fullTableFileName, encoding = 'utf-8') as fullTable:
-		headers = fullTable.readline().strip().split(",")
-		headers.remove(headers[0])
-		seasons = headers
-		#表头：项目A_季度&
-		keys = []
-		#数据：ValueOf(项目A_季度&)
-		values = []
-		for line in fullTable:
-			#单行内所有值
-			cells = line.strip().upper().split(",")
-			#行首作为项目名称
-			key = cells[0]
-			keys.append(key)
-			#单行内所有值移除行首作为所有数据值
-			cells.remove(key)
-			values = cells
+# 遍历股票代码
+stockCodes = os.listdir(foldPath)
+progress = ProgressBar(len(stockCodes), fmt=ProgressBar.FULL)
+for stockCode in stockCodes:
+    # 表头 => 数据
+    KeyValuePair = {}
+    # 将新生成的full表展开单行
+    fullTableFileName = foldPath + stockCode + "/" + stockCode + "_full.csv"
+    with open(fullTableFileName, encoding='utf-8') as fullTable:
+        headers = fullTable.readline().strip().split(",")
+        headers.remove(headers[0])
+        seasons = headers
+        # 表头：项目A_季度&
+        keys = []
+        # 数据：ValueOf(项目A_季度&)
+        values = []
+        for line in fullTable:
+            # 单行内所有值
+            cells = line.strip().upper().split(",")
+            # 行首作为项目名称
+            key = cells[0]
+            keys.append(key)
+            # 单行内所有值移除行首作为所有数据值
+            cells.remove(key)
+            values = cells
 
-			seasonIndex = 0
-			for value in values:
-				if (seasonIndex < len(seasons)):
-					KeyValuePair[key + "_" + str(seasons[seasonIndex])] = value
-					seasonIndex += 1
-	
-	expandTableHeadersLine = str()
-	expandTableValuesLine = str()
-	fullExpandTableFileName = foldPath + stockCode + "/" + stockCode + "_full_expand.csv"
-	with open(fullExpandTableFileName, "w", encoding = 'gbk') as file2Write:
-		expandTableHeadersLine += "股票代码"
-		expandTableValuesLine += str(stockCode)
-		for key,value in KeyValuePair.items():
-			expandTableHeadersLine += str("," + key)
-			expandTableValuesLine += str("," + value)
-		file2Write.writelines(expandTableHeadersLine + "\r\n")
-		file2Write.writelines(expandTableValuesLine)
-		print("已写入文件" + fullExpandTableFileName)
-print("执行完毕")
+            seasonIndex = 0
+            for value in values:
+                if (seasonIndex < len(seasons)):
+                    KeyValuePair[key + "_" + str(seasons[seasonIndex])] = value
+                    seasonIndex += 1
+
+    expandTableHeadersLine = str()
+    expandTableValuesLine = str()
+    fullExpandTableFileName = foldPath + stockCode + \
+        "/" + stockCode + "_full_expand.csv"
+    with open(fullExpandTableFileName, "w", encoding='gbk') as file2Write:
+        expandTableHeadersLine += "股票代码"
+        expandTableValuesLine += str(stockCode)
+        for key, value in KeyValuePair.items():
+            expandTableHeadersLine += str("," + key)
+            expandTableValuesLine += str("," + value)
+        file2Write.writelines(expandTableHeadersLine + "\r\n")
+        file2Write.writelines(expandTableValuesLine)
+    progress.current += 1
+    progress()
+progress.done()
